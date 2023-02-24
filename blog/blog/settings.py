ca2ebaa71fda620977f9c698a6597afc34afc903
Your Name <you@example.com>
2023-02-24 23:19:46 +0800

    '数据库配置‘

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'hpucjf', # 数据库用户名
+        'PASSWORD': '123456', # 数据库用户密码
+        'NAME': 'blog' # 数据库名字
+    },
 }
 
 
@@ -121,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
